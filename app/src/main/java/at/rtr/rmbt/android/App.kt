6b04dc211b9f272627010b26dcee876e54cf7d4e
David Furmánek <furmanekd@gmail.com>
2022-06-02 15:01:01 +0200

    Comment out maps related bugfix in App.kt

--- app/src/main/java/at/rtr/rmbt/android/App.kt
+++ app/src/main/java/at/rtr/rmbt/android/App.kt
@@ -46,24 +46,24 @@ class App : CoreApp() {
             WorkLauncher.enqueueSettingsRequest(this)
 
         // https://issuetracker.google.com/issues/154855417#comment367 Workaround
-        try {
-            val sharedPreferences = getSharedPreferences("google_bug_154855417", Context.MODE_PRIVATE)
-            if (!sharedPreferences.contains("fixed")) {
-                val corruptedZoomTables = File(filesDir, "ZoomTables.data")
-                val corruptedSavedClientParameters = File(filesDir, "SavedClientParameters.data.cs")
-                val corruptedClientParametersData = File(filesDir, "DATA_ServerControlledParametersManager.data.$packageName")
-                val corruptedClientParametersDataV1 = File(filesDir, "DATA_ServerControlledParametersManager.data.v1.$packageName")
-                corruptedZoomTables.delete()
-                corruptedSavedClientParameters.delete()
-                corruptedClientParametersData.delete()
-                corruptedClientParametersDataV1.delete()
-                sharedPreferences.edit().putBoolean("fixed", true).apply()
-            }
-        } catch (exception: Exception) {
-        }
-
-        val config = AGConnectServicesConfig.fromContext(this)
-        MapsInitializer.setApiKey(config.getString("client/api_key"))
+//        try {
+//            val sharedPreferences = getSharedPreferences("google_bug_154855417", Context.MODE_PRIVATE)
+//            if (!sharedPreferences.contains("fixed")) {
+//                val corruptedZoomTables = File(filesDir, "ZoomTables.data")
+//                val corruptedSavedClientParameters = File(filesDir, "SavedClientParameters.data.cs")
+//                val corruptedClientParametersData = File(filesDir, "DATA_ServerControlledParametersManager.data.$packageName")
+//                val corruptedClientParametersDataV1 = File(filesDir, "DATA_ServerControlledParametersManager.data.v1.$packageName")
+//                corruptedZoomTables.delete()
+//                corruptedSavedClientParameters.delete()
+//                corruptedClientParametersData.delete()
+//                corruptedClientParametersDataV1.delete()
+//                sharedPreferences.edit().putBoolean("fixed", true).apply()
+//            }
+//        } catch (exception: Exception) {
+//        }
+//
+//        val config = AGConnectServicesConfig.fromContext(this)
+//        MapsInitializer.setApiKey(config.getString("client/api_key"))
     }
 
     override fun attachBaseContext(base: Context?) {
